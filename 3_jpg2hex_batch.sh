7e5670b8bcbc09f76225693f85eb26c2c37aae0a
Superbony-the-Cat <89550084+Superbony-the-Cat@users.noreply.github.com>
2024-11-08 01:22:28 +0100

    Update 3_jpg2hex_batch.sh
    filename update       "jpg2hex_one_Pic.sh"

--- 3_jpg2hex_batch.sh
+++ 3_jpg2hex_batch.sh
@@ -2,6 +2,7 @@
 
 for file in *.jpg;
 
-do ./bin2hex_jpg2hex.sh "$file" > "${file%%.jpg}.h";   # $./bin2hex_jpg2hex.sh 01.jpg > p01.h
+#  do ./bin2hex_jpg2hex.sh "$file" > "${file%%.jpg}.h";   # $./bin2hex_jpg2hex.sh 01.jpg > p01.h
+do ./jpg2hex_one_Pic.sh "$file" > "${file%%.jpg}.h";   # $./jpg2hex_one_Pic.sh 01.jpg > p01.h
 
 done
